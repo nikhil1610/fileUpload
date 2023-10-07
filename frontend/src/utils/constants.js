e5dab0e01a19eb0aba21c77ac03922f713780528
nikhil1610 <nikhilgautam106@gmail.com>
2023-10-07 20:18:57 +0530

    url set to prod

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_URL='http://localhost:3030';
-// export const API_URL='https://bytesize-url.onrender.com';
+// export const API_URL='http://localhost:3030';
+export const API_URL='https://bytesize-url.onrender.com';
